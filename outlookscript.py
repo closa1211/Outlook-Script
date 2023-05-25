991a737493cbe6927c347aa039b806e9a0eef419
Carlos <camaral0355@gmail.com>
2023-05-25 10:54:33 -0400

    updated .reg file and script

--- outlookscript.py
+++ outlookscript.py
@@ -4,10 +4,13 @@ import sys
 import win32com.client
 
 def open_outlook_with_file_path(file_path):
+    # Extract the file name from the file path
+    file_name = os.path.basename(file_path)
+
     # Open Outlook and create a new email
     outlook = win32com.client.Dispatch("Outlook.Application")
     mail_item = outlook.CreateItem(0)
-    mail_item.Subject = "[FOR REVIEW]"
+    mail_item.Subject = f"[FOR REVIEW] {file_name}"
 
     #Format the file as a hyperlink and paste it in the email
     file_link = f"<a href='file://{file_path}'>{file_path}</a>"
